7b60b0dc98bfa945e03f315ea3e637f5c520dbfb
Minha Ahn <dks4857@gmail.com>
2025-01-20 16:31:35 +0900

    💄 style: 스타일 가이드 세팅 (#19)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,42 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: '#FFFFFF',
+        black: '#000000',
+        gray: {
+          5: '#F2F2F2',
+          10: '#E7E7E7',
+          15: '#DADADA',
+          20: '#CECECE',
+          30: '#B6B6B6',
+          40: '#9D9D9D',
+          50: '#858585',
+          60: '#6C6C6C',
+          70: '#545454',
+          80: '#3B3B3B',
+          90: '#232323',
+          100: '#0A0A0A',
+        },
+        primary: {
+          1: '#1F2937',
+          2: '#183F96',
+          3: '#4BAFFF',
+          4: '#90CAF9',
+          5: '#DBEAFE',
+          hover: '#364861',
+        },
+        secondary: {
+          1: '#E3F2FD',
+          2: '#F5F9FF',
+          3: '#F8FAFC',
+        },
+        accent: {
+          error: '#EB2525',
+        },
+      },
+    },
   },
   plugins: [],
 };
