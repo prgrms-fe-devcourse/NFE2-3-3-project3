337e231f2b2381052bf276d2fa3fa8e23e371f78
Hyunw00 <96560273+kodingzz@users.noreply.github.com>
2025-01-21 10:42:23 +0900

    Style modal tab menu #31 (#34)
    * 🎨  Design: 공통 모달창 퍼블리싱
    
    * 🎨 Style: 탭 메뉴 공통 컴포넌트 퍼블리싱
    
    * 🚨 Fix: 공통 모달, 탭 메뉴 스타일링 수정
    
    * 💄 Style: 탭 메뉴 활성화 스타일 링 적용

---
+++ src/components/TabMenu.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { computed, ref } from 'vue';
+
+const props = defineProps({
+  menuItems: {
+    type: Array,
+    required: true,
+  },
+  activeIndex: {
+    type: Number,
+    required: true,
+  },
+});
+
+const emit = defineEmits(['update:activeIndex']);
+</script>
+<template>
+  <div class="w-full flex flex-col justify-center items-center h-[50px] py-[18px] bg-secondary-1">
+    <nav>
+      <ul class="body-large-m inline-flex items-center gap-[30px] text-gray-80">
+        <li
+          v-for="(item, index) in props.menuItems"
+          :key="index"
+          class="cursor-pointer"
+          @click="emit('update:activeIndex', index)"
+          :class="props.activeIndex === index && 'text-primary-2'"
+        >
+          {{ item }}
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+<style scoped></style>
--- src/pages/MyPage.vue
+++ src/pages/MyPage.vue
@@ -1,5 +1,15 @@
-<script setup></script>
+<script setup>
+import TabMenu from '@/components/TabMenu.vue';
+import { ref } from 'vue';
+
+const activeIndex = ref(0);
+</script>
 
 <template>
   <div>마이 페이지</div>
+  <TabMenu
+    :menu-items="['내정보', '작성한 글', '찜 목록']"
+    :activeIndex="activeIndex"
+    @update:activeIndex="activeIndex = $event"
+  />
 </template>
