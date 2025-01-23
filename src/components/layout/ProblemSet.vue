9c8d2926a5e54d0213cbfe93f9f11939cd4694ee
quokka <ansagi3@naver.com>
2025-01-23 15:22:31 +0900

    🐛 초기값 타입 불일치 해결 & 옵셔널체이닝 추가

--- src/components/layout/ProblemSet.vue
+++ src/components/layout/ProblemSet.vue
@@ -12,19 +12,19 @@ const { problemSet } = defineProps({
       class="flex flex-col justify-between w-full h-[146px] bg-orange-3 gap-[50px] px-4 py-3 rounded-lg"
     >
       <div>
-        <p class="font-pretend text-lg">{{ problemSet.title }}</p>
-        <p class="text-sm text-gray-1">{{ problemSet.description }}</p>
+        <p class="font-pretend text-lg">{{ problemSet?.title }}</p>
+        <p class="text-sm text-gray-1">{{ problemSet?.description }}</p>
       </div>
       <div class="flex justify-between items-center">
         <div class="flex items-center">
           <Avatar
-            :image="problemSet.user.avatar_url"
+            :image="problemSet?.user.avatar_url"
             class="mr-2 max-w-7 max-h-7"
             shape="circle"
           />
-          <span>{{ problemSet.user.name }}</span>
+          <span>{{ problemSet?.user.name }}</span>
         </div>
-        <p class="font-medium">{{ problemSet.count }}문제</p>
+        <p class="font-medium">{{ problemSet?.count }}문제</p>
       </div>
     </article>
   </RouterLink>
