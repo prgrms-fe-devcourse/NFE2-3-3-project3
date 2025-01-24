64e965c1f2695d7a01f281b95977734e15a21468
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2025-01-24 21:54:58 +0900

    🚨 Fix: Sort가 초기화 이전에 사용돼서 페이지가 안 열리는 버그 수정

--- src/const/sorts.js
+++ src/const/sorts.js
@@ -1,9 +1,9 @@
-export const SORTS = [
-  { name: SORT.latest, value: SORT.latest },
-  { name: SORT.likes, value: SORT.likes },
-];
-
 export const SORT = {
   latest: "최신순",
   likes: "좋아요 많은 순",
 };
+
+export const SORTS = [
+  { name: SORT.latest, value: SORT.latest },
+  { name: SORT.likes, value: SORT.likes },
+];
