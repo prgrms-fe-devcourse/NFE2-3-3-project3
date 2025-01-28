fdb348b0660d76b019b1b65b35d693cadb1805cf
YongjaeHyun <dltjrrbs2020@gmail.com>
2025-01-29 03:21:22 +0900

    🚨 Fix: 포인트 내역의 정렬을 내림차순으로 변경

--- src/api/point.js
+++ src/api/point.js
@@ -5,7 +5,8 @@ const getAll = async (userId) => {
     const { data, error } = await supabase
       .from("point")
       .select("*")
-      .eq("uid", userId);
+      .eq("uid", userId)
+      .order("created_at", { ascending: false });
 
     if (error) throw error;
     return data;
