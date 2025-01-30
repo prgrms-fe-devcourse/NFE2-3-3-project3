cc52edb119dc17a35f55f999614374b8925bf4a8
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2025-01-31 04:39:45 +0900

    ✨ Feature: 정렬 시에, 라우팅 동작을 push에서 replace로 변경

--- src/components/layout/ProblemTable.vue
+++ src/components/layout/ProblemTable.vue
@@ -232,7 +232,7 @@ const sortedProblems = computed(() => {
 
 watch(sort, (newSort) => {
   const newQuery = { ...route.query, sort: newSort.value };
-  router.push({ query: newQuery });
+  router.replace({ query: newQuery });
 });
 
 watchEffect(async () => {
