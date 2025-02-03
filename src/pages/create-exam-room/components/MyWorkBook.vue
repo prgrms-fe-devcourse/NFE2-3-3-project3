04286fa3554cb89aa1be9e30f53fbee1a0e79f4c
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2025-02-03 10:36:56 +0900

    ✨ Feature: 상세페이지에서 시험치기 버튼으로 들어왔을 때, 자동으로 해당 문제집이 선택되도록 구현

--- src/pages/create-exam-room/components/MyWorkBook.vue
+++ src/pages/create-exam-room/components/MyWorkBook.vue
@@ -1,9 +1,10 @@
 <script setup>
 import shareIcon from "@/assets/icons/my-problem-sets/share.svg";
-import { watchEffect, ref } from "vue";
+import { ref, watchEffect } from "vue";
 import { workbookAPI } from "@/api/workbook.js";
 
-const props = defineProps({
+const { visibleMyBooks, selectedWorkbook } = defineProps({
+  selectedWorkbook: Object,
   visibleMyBooks: {
     required: true,
   },
@@ -17,25 +18,25 @@ const handleSelect = (book) => {
 };
 
 watchEffect(async () => {
-  if (props.visibleMyBooks) {
-    books.value = await Promise.all(
-      props.visibleMyBooks.map(async (book) => {
-        try {
-          const count = await workbookAPI.getWorkbookProblemCount(book.id);
-          return {
-            ...book,
-            problem_count: count,
-          };
-        } catch (error) {
-          console.error(`문제집 ${book.id}의 문제 수 조회 실패:`, error);
-          return {
-            ...book,
-            problem_count: 0,
-          };
-        }
-      }),
-    );
-  }
+  if (!visibleMyBooks) return;
+
+  books.value = await Promise.all(
+    visibleMyBooks.map(async (book) => {
+      try {
+        const count = await workbookAPI.getWorkbookProblemCount(book.id);
+        return {
+          ...book,
+          problem_count: count,
+        };
+      } catch (error) {
+        console.error(`문제집 ${book.id}의 문제 수 조회 실패:`, error);
+        return {
+          ...book,
+          problem_count: 0,
+        };
+      }
+    }),
+  );
 });
 </script>
 
@@ -44,7 +45,10 @@ watchEffect(async () => {
     <div
       v-for="book in books"
       :key="book.id"
-      class="w-[calc((100% - 4rem) / 3)] h-[146px] p-4 bg-orange-3 rounded-lg flex flex-col justify-between cursor-pointer hover:bg-orange-200 transition-colors"
+      :class="[
+        'w-[calc((100% - 4rem) / 3)] h-[146px] p-4 bg-orange-3 rounded-lg flex flex-col justify-between cursor-pointer hover:bg-orange-200 transition-colors',
+        selectedWorkbook?.id === book.id ? 'border-2 border-orange-1' : '',
+      ]"
       @click="handleSelect(book)"
     >
       <div>
@@ -56,7 +60,7 @@ watchEffect(async () => {
       <div class="flex justify-between items-center mt-2">
         <img v-if="book.shared" :src="shareIcon" alt="share" />
         <span v-else class="w-6"></span>
-        <p class=" font-medium">{{ book.problem_count || 0 }}문제</p>
+        <p class="font-medium">{{ book.problem_count || 0 }}문제</p>
       </div>
     </div>
   </div>
