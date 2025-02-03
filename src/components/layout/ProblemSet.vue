893514cdf4bdaeda91f671c49e71fc0e4ac040c1
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2025-02-03 14:46:36 +0900

    🚨 Fix: 문제집 링크 범위가 컨텐츠 범위를 넘어가는 버그 수정

--- src/components/layout/ProblemSet.vue
+++ src/components/layout/ProblemSet.vue
@@ -7,9 +7,9 @@ const { problemSet } = defineProps({
 });
 </script>
 <template>
-  <RouterLink>
+  <RouterLink class="max-h-40">
     <article
-      class="flex flex-col justify-between w-full h-40 bg-orange-3 gap-[50px] px-4 py-3 rounded-lg"
+      class="flex flex-col justify-between w-full h-full bg-orange-3 gap-[50px] px-4 py-3 rounded-lg"
     >
       <div class="min-h-12">
         <p class="font-pretend text-lg line-clamp-1 break-all">
