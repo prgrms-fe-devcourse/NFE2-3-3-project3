c95a8a7b31719bafa5edc4c3a8fabd9cc2053195
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2025-02-03 11:42:04 +0900

    💄 Style: 시험 결과 페이지 디자인 수정

--- src/pages/exam-result/components/ExamResultHeader.vue
+++ src/pages/exam-result/components/ExamResultHeader.vue
@@ -53,24 +53,25 @@ watch(
 initializeAuth();
 </script>
 <template>
-  <header class="w-full pl-40 pt-20 pb-10 relative">
-    <div class="flex items-center gap-8">
-      <div class="flex flex-col items-center justify-center space-y-1">
-        <img
-          :src="profileImage"
-          alt="Profile"
-          class="rounded-full w-32 h-32 object-cover"
-        />
-        <span class="font-bold text-base pt-2">{{ userName }}</span>
-        <span class="text-sm text-gray-500">{{ userEmail }}</span>
-      </div>
-
-      <div class="pb-16 space-y-1">
-        <h1 class="text-xl font-semibold">{{ workbookTitle }}</h1>
-        <p class="font-laundry text-3xl">시험 결과 리포트</p>
-        <p class="text-sm text-gray-500">
-          시험 결과 리포트는 지난 시험에서 다시 확인 할 수 있습니다.
-        </p>
+  <header class="w-full pt-20 pb-10 h-full">
+    <div class="flex items-center w-fit gap-8 mx-auto">
+      <div class="flex items-center gap-8 mr-[30rem]">
+        <div class="flex flex-col items-center justify-center space-y-1">
+          <img
+            :src="profileImage"
+            alt="Profile"
+            class="rounded-full w-32 h-32 object-cover"
+          />
+          <span class="font-bold text-base pt-2">{{ userName }}</span>
+          <span class="text-sm text-gray-500">{{ userEmail }}</span>
+        </div>
+        <div class="pb-16 space-y-1">
+          <h1 class="text-xl font-semibold">{{ workbookTitle }} 시험장</h1>
+          <p class="font-laundry text-3xl font-semibold">시험 결과 리포트</p>
+          <p class="text-sm text-gray-500">
+            시험 결과 리포트는 지난 시험에서 다시 확인 할 수 있습니다.
+          </p>
+        </div>
       </div>
     </div>
   </header>
